9a069f51b03424feafc175f1f4b5de40ee696846
edougher <ejdougher@gmail.com>
2020-12-08 01:04:14 -0600

    Done

--- run.rb
+++ run.rb
@@ -7,31 +7,66 @@ spicy_foods = [
 ]
 
 def print_spicy_foods(spicy_foods)
-
-end
+  spicy_foods.each do |v|
+   puts "#{v[:name]} (#{v[:cuisine]}) | Heat level: 🌶🌶🌶 \n"
+  end
+ end
+print_spicy_foods(spicy_foods)
 
 def get_names(spicy_foods)
-
+ new_array = []
+spicy_foods.each do |names|
+# puts names.values[0]
+new_array << [names[:name]]
+#binding.pry
+return new_array
 end
+end
+get_names(spicy_foods)
 
 def spiciest_foods(spicy_foods)
+new_array = []
+spicy_foods.select do |n|
+  if n[:heat_level] > 5
+    new_array << ["#{n[:name]} Heat Level: #{n[:heat_level]}"]
+    #binding.pry
+    return new_array
 
+  end
 end
+end
+spiciest_foods(spicy_foods)
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-
+  spicy_foods.select do |n|
+     if cuisine == n[:cuisine]
+     puts n[:name]
+     end
+  end
 end
+#get_spicy_food_by_cuisine(spicy_foods, "Sichuan")
 
 # BONUS Deliverables
 def print_spiciest_foods(spicy_foods)
-
+  new_array = []
+  spicy_foods.select do |n|
+    if n[:heat_level] > 5
+      new_array << ["#{n[:name]} Heat Level: #{n[:heat_level]}"]
+      #binding.pry
+      return new_array
+  
+    end
+  end
 end
+#print_spiciest_foods(spicy_foods)
 
 def average_heat_level(spicy_foods)
-
+heat = 0
+spicy_foods.each do |h|
+  heat += h[:heat_level]
 end
-
+return heat/3
+end
+average_heat_level(spicy_foods)
 # Use this to test your methods
 # run "ruby run.rb" and try calling the methods from the console
-binding.pry
-"pls"
